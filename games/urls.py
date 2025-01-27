d2ba6ca3b9a66230f3add67c85dc4c4550197784
marina <145054405+mdiogc@users.noreply.github.com>
2025-01-27 19:32:48 +0000

    Update urls.py

--- games/urls.py
+++ games/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'games'
 
 urlpatterns = [
-    path('', views.games_list, name='games-list'),
-    path('filter/', views.games_list, name='filter'),
+    path('', views.game_list, name='game-list'),
+    path('filter/', views.game_list, name='filter'),
     path('<str:title>/', views.games_detail, name='games-detail'),
     path('<str:title>/reviews/', views.review_list, name='review-list'),
     path('<str:title>/reviews/add/', views.add_review, name='add-review'),
